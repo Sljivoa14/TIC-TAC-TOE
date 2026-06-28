9807b373cd392b37d76ba3db6948c23e7e09d966
Sljivoa14 <amarsljivo7@gmail.com>
2026-06-28 17:55:59 +0200

    tic tac toe pt2

--- SCRIPTS/script.js
+++ SCRIPTS/script.js
@@ -1,21 +1,57 @@
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getConext('2d');
+const statusTxt = document.getElementById('status');
+const resetBtn = document.getElementById('reset');
 
-const canvasEmpty = [
-    "", "", "",
-    "", "", "",
-    "", "", ""
-
+let canvasBoard =[
+    ["", "", ""],
+    ["", "", ""]
+    ["", "", ""]
 ]
 
+canvasBoard.width = 300;
+canvasBoard.height = 300;
 const cellSize = 100;
 
 let currentPlayer = "X";
+let gameOver = false;
 canvas.addEventListener('click', handleClick);
+resetBtn.addEventListener('click', resetGame);
+
+function draw(){
+    for(let i = 0; i < 3; i++){
+        for(let j = 0; j < 3; j++){
+            const x = i * cellSize;
+            const y = j * cellSize;
+            ctx.strokeRect(x, y, cellSize, cellSize);
+
+            if( canvasBoard[i][j] === ""){
+                ctx.font = "70px arial";
+                ctx.textAlign = "centar";            
+                ctx.fillText(canvasBoard[i][j], x + 30, y +70)};
+        }
+    }
+}
 
 function handleClick(event){
     const x = Math.floor(event.offsetX / cellSize);
     const y = Math.floor(event.offsetY / cellSize);
 
-    
+
 }
+
+function checkWinner(){
+
+}
+function resetGame(){
+    canvasBoard = [
+    "", "", "",
+    "", "", "",
+    "", "", ""
+
+]
+currentPlayer = "X";
+draw();
+}
+
+draw();
